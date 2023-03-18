95a13625e1f0c0f8821a8fce6169001f0e9e5e88
twwold <122291133+twwold@users.noreply.github.com>
2023-03-18 16:13:03 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,30 +15,30 @@ global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
 global.ANTI_BADWORD = 'true' // true of false
-global.AUTO_REACT = 'false' // true of false
-global.INBOX_BLOCK_MSG ='BLOCK !!!'
-global.INBOX_BLOCK = 'off' //inbox block
-global.alivemsg = 'ᴅᴀʀᴋ-ɴᴇʀᴏ ʙᴏᴛ'
-global.alivepic = 'https://i.ibb.co/zFYBPcQ/IMG-20230226-WA0115.jpg '
-global.owner = ['0786802371'] //ur owner number
-global.ownername = "kaveesha" //ur owner name
-global.ytname = "YT: Dark" //ur yt chanel name
-global.socialm = "GitHub: Dark" //ur github or insta name
+global.AUTO_REACT = 'true' // true of false
+global.INBOX_BLOCK_MSG ='BLOCK !!! || https://chat.whatsapp.com/IX00RyPG07d7i5lvBoekcc || 𝙅𝙊𝙄𝙉©'
+global.INBOX_BLOCK = 'on' //inbox block
+global.alivemsg = '|| https://chat.whatsapp.com/IX00RyPG07d7i5lvBoekcc || 𝙅𝙊𝙄𝙉©'
+global.alivepic = 'https://telegra.ph/file/62333464ba8d6a28e1f4f.jpg '
+global.owner = ['07862912371'] //ur owner number
+global.ownername = "TWORLD" //ur owner name
+global.ytname = "YT: Technoland" //ur yt chanel name
+global.socialm = "GitHub: Technoland_World" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.session = " " //session Id
-global.ownernomer = "94786802371" //ur number
+global.session = "dHBBZERKdFc= " //session Id
+global.ownernomer = "947862912371" //ur number
 global.lang = "SI" //Select Language "EN" to English "SI" to sinhala 
-global.premium = ['94786802371'] //ur premium number
-global.botname = 'ᴅᴀʀᴋ-ɴᴇʀᴏ ᴡʜᴀᴛʜᴀᴘᴘ ʙᴏᴛ' //ur bot name
-global.linkz = "https://chat.whatsapp.com/HYj9wu5Jrv6CROxyeQbHoS" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://www.darknero.ga/' //script link
+global.premium = ['947862912371'] //ur premium number
+global.botname = '꧁⃟тωσяℓ∂⃢м∂вσт〠⃤꧂' //ur bot name
+global.linkz = "https://chat.whatsapp.com/IX00RyPG07d7i5lvBoekcc" //your theme url which will be displayed on whatsapp
+global.websitex = "https://youtu.be/WinePwsDjFo" //ur website to be displayed
+global.botscript = 'https://www.Technow.ga/' //script link
 global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Dark Nero By" //ur sticker watermark packname
-global.author = "Dark Nero Creating By Cyber Yakuza Team" //ur sticker watermark author
-global.wm = "Dark Bot Inc." //ur watermark
+global.packname = "Technolandworldbot By" //ur sticker watermark packname
+global.author = "Tworldmdbot Creating By Technolandworldbot" //ur sticker watermark author
+global.wm = "Technolandworld Inc." //ur watermark
 
 // Other
 global.sessionName = 'session'
